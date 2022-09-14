83c8355e7cc043963509e8ffe7933124468e488b
Damien-Warmouth <damienwarmouth@gmail.com>
2022-09-14 09:10:24 -0500

    Frontend remake of consoleResume on /Console

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route}
+from 'react-router-dom';
 import './App.css';
+import Console from './pages/Console';
+import Home from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+	render() {
+		return (
+			<div >
+			<Router>
+			<Routes>
+				<Route exact path='/' exact element={<Home/>} />
+				<Route path='/Console' element={<Console/>} />
+			</Routes>
+			</Router>
+			</div>
+		);
+		
+	}
 }
 
 export default App;
---
+++ src/pages/Console.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Console.css';
+
+const Console = () => {
+  return (
+    <div>
+        <h1>
+            Hello
+        </h1>
+    </div>
+  )
+}
+
+export default Console
